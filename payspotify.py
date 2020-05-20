1ef873591e598443109a12270548b0aefeb6da00
Nissara <65638665+Nissara@users.noreply.github.com>
2020-05-20 15:05:35 +0700

    Update payspotify.py

--- payspotify.py
+++ payspotify.py
@@ -1,3 +1,13 @@
-print('apple')
-print('banana')
-print('frout')
+from flask import Flask, request, abort
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello Guys!"
+
+
+@app.route("/webhook")
+def webhook():
+    return 'OK'
+    
+if __name__ == "__main__":
+    app.run()
